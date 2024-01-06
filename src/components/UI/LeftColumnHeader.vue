6a57cb612c654112bb6aa0194d7180a1b620ce2b
Bjoref <flamros@yandex.ru>
2024-01-06 13:49:10 +0500

    initial commit

---
+++ src/components/UI/LeftColumnHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="header__content">
+    <img class="header__img" src="../../img/photo.jpg" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'leftColumnHeader',
+}
+</script>
+
+<style scoped>
+.header__content img {
+  width: 100px;
+  height: 134px;
+  border-radius: 15px;
+}
+
+.header__content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px 0;
+}
+</style>
